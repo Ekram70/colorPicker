6d567ec1c55d5297f46cc399f0312ab1faf0c82a
Ekram70 <ekramullah70@gmail.com>
2022-04-26 00:01:22 +0600

    button added to copy the color code

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 /**
  * Change the background color by generating random rbg color by clicking a button
  * Also display the hex code to the disabled input field
+ * Add a button to copy the color code
  */
 
 // Step 1 - create onload handler
@@ -13,11 +14,16 @@ function main() {
   const root = document.getElementById("root");
   const btn = document.getElementById("change-btn");
   const output = document.getElementById("output");
+  const copyBtn = document.getElementById("copy-btn");
 
   btn.addEventListener("click", function () {
     const bgColor = generateHexColor();
     root.style.backgroundColor = bgColor;
     output.setAttribute("value" , `${bgColor}`);
+  });
+  
+  copyBtn.addEventListener("click", function (){
+    navigator.clipboard.writeText(output.getAttribute("value"));
   });
 }
 
