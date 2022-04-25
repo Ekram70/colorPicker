b94a32cd7c8d2f5db31acea87c79267db6ef8b5f
Ekram70 <ekramullah70@gmail.com>
2022-04-26 01:28:55 +0600

    show rgb color and user can copy

--- main.js
+++ main.js
@@ -4,6 +4,8 @@
  * Add a button to copy the color code
  * Add a toast message when copied
  * User can type their own hex code
+ * Remove the # from input of hex
+ * Show rgb color and user can copy
  */
 
 // Step 1 - create onload handler
@@ -16,39 +18,84 @@ function main() {
   const root = document.getElementById("root");
   const btn = document.getElementById("change-btn");
   const output = document.getElementById("output");
+  const output2 = document.getElementById("output2");
   const copyBtn = document.getElementById("copy-btn");
+  const copyBtn2 = document.getElementById("copy-btn2");
 
   btn.addEventListener("click", function () {
-    const bgColor = generateHexColor();
-    root.style.backgroundColor = bgColor;
-    output.setAttribute("value", `${bgColor}`);
-    output.value = bgColor.slice(1);
+    const color = decimalColor();
+    const hexColor = generateHexColor(color);
+    root.style.backgroundColor = hexColor;
+    output.setAttribute("value", `${hexColor.slice(1)}`);
+    output.value = hexColor.slice(1);
+
+    const rbgColor = generateRgbColor(color);
+    output2.setAttribute("value", `${rbgColor}`);
+    output2.value = rbgColor;
   });
 
   copyBtn.addEventListener("click", function () {
     navigator.clipboard.writeText(output.getAttribute("value"));
-    generateToastMessage(`${output.getAttribute("value").toUpperCase()} is Copied`);
+    if( isValidHex(output.getAttribute("value")) ){
+      generateToastMessage(`${output.getAttribute("value").toUpperCase()} is Copied`);
+    }
+    else{
+      generateToastMessage(`Invalid Color Code!`);
+    }
   });
+
+  copyBtn2.addEventListener("click", function () {
+    navigator.clipboard.writeText(output2.getAttribute("value"));
+    if( isValidHex(output.getAttribute("value")) ){
+      generateToastMessage(`${output2.getAttribute("value").toUpperCase()} is Copied`);
+    }
+    else{
+      generateToastMessage(`Invalid Color Code!`);
+    }
+  });
+
+
 
   output.addEventListener("keyup", function(event){
       const color = event.target.value;
       if( isValidHex(color) ){
         root.style.backgroundColor = `#${color}`;
-        output.setAttribute("value", `#${color}`);
+        output.setAttribute("value", `${color}`);
+
+        output2.setAttribute("value", `${hexToDecimal(color)}`);
+        output2.value = hexToDecimal(color);
       }
+
   });
 
 }
 
 // step 2 - random color generator function
 
-function generateHexColor() {
-  const red = Math.floor(Math.random() * 255).toString(16);
-  const green = Math.floor(Math.random() * 255).toString(16);
-  const blue = Math.floor(Math.random() * 255).toString(16);
-  return `#${red}${green}${blue}`;
+function decimalColor(){
+  const red = Math.floor(Math.random() * 255);
+  const green = Math.floor(Math.random() * 255);
+  const blue = Math.floor(Math.random() * 255);
+  return {red,green,blue};
 }
 
+function generateHexColor(color) {
+  function twoCodeColor(x){
+      return x.toString(16).length == 1 ? `0${x.toString(16)}`: x.toString(16);
+  }
+  return `#${twoCodeColor(color.red)}${twoCodeColor(color.green)}${twoCodeColor(color.blue)}`;
+}
+
+function generateRgbColor(color){
+  return `Rgb(${color.red},${color.green},${color.blue})`;
+}
+
+function hexToDecimal(hex){
+    const red = parseInt(hex.slice(0,2), 16);
+    const green = parseInt(hex.slice(2,4), 16);
+    const blue = parseInt(hex.slice(4), 16);
+    return `Rgb(${red},${green},${blue})`
+}
 // step 3 - toast message generator function
 
 function generateToastMessage(msg) {
