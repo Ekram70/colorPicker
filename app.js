e85dd6f199c8e9f59e157f9f190c481b0b82f846
Ekram70 <ekramullah70@gmail.com>
2022-04-27 21:25:14 +0600

    jsdoc added for documentation

--- app.js
+++ app.js
@@ -23,29 +23,40 @@ function main() {
   generateRandomColor.addEventListener("click", handleRandomColor);
   copyToClipBoard.addEventListener("click", handleCopyToClipBoard);
   inputHex.addEventListener("keyup", handleInputHex);
+
+  // sliders event
   colorSliderRed.addEventListener(
     "input",
+
     handleColorSlider(colorRedLabel, colorSliderRed)
   );
   colorSliderGreen.addEventListener(
     "input",
+
     handleColorSlider(colorGreenLabel, colorSliderGreen)
   );
   colorSliderBlue.addEventListener(
     "input",
+
     handleColorSlider(colorBlueLabel, colorSliderBlue)
   );
 
   // callback function for event handlers
+
+  /**
+   * handles the click event of random color
+   */
   function handleRandomColor() {
     const color = decimalColor();
     const hexColor = generateHexColor(color);
     colorDisplay.style.backgroundColor = hexColor;
     inputHex.setAttribute("value", `${hexColor.slice(1)}`);
+
     inputHex.value = hexColor.slice(1);
 
     const rbgColor = generateRgbColor(color);
     inputRgb.setAttribute("value", `${rbgColor}`);
+
     inputRgb.value = rbgColor;
 
     colorRedLabel.innerText = color.red;
@@ -53,10 +64,14 @@ function main() {
     colorBlueLabel.innerText = color.blue;
 
     colorSliderRed.value = color.red;
+
     colorSliderGreen.value = color.green;
     colorSliderBlue.value = color.blue;
   }
 
+  /**
+   * handles the copy to clipboard click event
+   */
   function handleCopyToClipBoard() {
     const checkedRadio = isCheckedRadio();
     if (checkedRadio == "hex") {
@@ -80,6 +95,9 @@ function main() {
     }
   }
 
+  /**
+   * handles the user input of hex fields keyup event
+   */
   function handleInputHex(event) {
     const color = event.target.value;
     if (isValidHex(color)) {
@@ -100,11 +118,15 @@ function main() {
       colorSliderRed.value = red;
       colorSliderGreen.value = green;
       colorSliderBlue.value = blue;
-
-      console.log(color);
     }
   }
 
+  /**
+   * creates a function that takes label and slide as parameter and returns a function to handel slider event
+   * @param {HTMLElement} label
+   * @param {HTMLElement} slider
+   * @returns {Function} function
+   */
   function handleColorSlider(label, slider) {
     return function () {
       const red = colorSliderRed.value;
@@ -129,6 +151,10 @@ function main() {
 
 // Utilities functions
 
+/**
+ * generates color object in number format
+ * @returns {Object} {red, green, blue}
+ */
 function decimalColor() {
   const red = Math.floor(Math.random() * 255);
   const green = Math.floor(Math.random() * 255);
@@ -136,6 +162,14 @@ function decimalColor() {
   return { red, green, blue };
 }
 
+/**
+ * takes a color object and generates hex color
+ * @param {Object} color
+ * @param {number} color.red
+ * @param {number} color.green
+ * @param {number} color.blue
+ * @returns {string} hex color with #
+ */
 function generateHexColor(color) {
   function twoCodeColor(x) {
     return x.toString(16).length == 1 ? `0${x.toString(16)}` : x.toString(16);
@@ -145,10 +179,23 @@ function generateHexColor(color) {
   )}`;
 }
 
+/**
+ * takes a color object and generates rgb color
+ * @param {Object} color
+ * @param {number} color.red
+ * @param {number} color.green
+ * @param {number} color.blue
+ * @returns {string} rgb color with braces
+ */
 function generateRgbColor(color) {
   return `Rgb(${color.red},${color.green},${color.blue})`;
 }
 
+/**
+ * converts hex to rgb
+ * @param {string} hex
+ * @returns {string} rgb color with braces
+ */
 function hexToDecimal(hex) {
   const red = parseInt(hex.slice(0, 2), 16);
   const green = parseInt(hex.slice(2, 4), 16);
@@ -156,6 +203,10 @@ function hexToDecimal(hex) {
   return `Rgb(${red},${green},${blue})`;
 }
 
+/**
+ * takes message as input and creates toast message
+ * @param {string} msg
+ */
 function generateToastMessage(msg) {
   let div = document.createElement("div");
   div.innerText = msg;
@@ -166,6 +217,11 @@ function generateToastMessage(msg) {
   }, 500);
 }
 
+/**
+ * takes hex color as input and validated the color
+ * @param {string} color
+ * @returns {boolean}
+ */
 function isValidHex(color) {
   if (color.length == 6) {
     return /^[0-9A-Fa-f]{6}$/i.test(color);
@@ -174,6 +230,10 @@ function isValidHex(color) {
   }
 }
 
+/**
+ * determines which radio button or mode is checked
+ * @returns {string} "hex" or "rgb"
+ */
 function isCheckedRadio() {
   let checked = "";
   const colorMode = document.getElementsByClassName("color-mode");
@@ -185,6 +245,9 @@ function isCheckedRadio() {
   return checked;
 }
 
+/**
+ * sets the default color when the window is loaded
+ */
 function defaultColor() {
   const color = {
     red: 221,
@@ -195,6 +258,7 @@ function defaultColor() {
   const rgbColor = generateRgbColor(color);
 
   document.getElementById("color-display").style.backgroundColor = hexColor;
+
   document.getElementById("input-hex").value = hexColor;
   document.getElementById("input-rgb").value = rgbColor;
   document.getElementById("input-hex").setAttribute("value", hexColor);
